2ff47a92735ba9b6952aefd50f78beb252884cea
nh0ckjapan <31547509+nh0ckjapan@users.noreply.github.com>
2018-10-23 23:51:53 +0700

    Lao lap Project
    Lao lap Project

---
+++ MyApp/MyApp/Views/HomePage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MyApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.MySplitView.IsPaneOpen = !this.MySplitView.IsPaneOpen;
+            if (!this.MySplitView.IsPaneOpen)
+            {
+                this.StackIcon.Margin = new Thickness(10, 50, 0, 0);
+            }
+            else
+            {
+                this.StackIcon.Margin = new Thickness(50, 50, 0, 0);
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+            switch (radio.Tag.ToString())
+            {
+                case "Register":
+                    this.MainFrame.Navigate(typeof(MainPage));
+                    break;
+                case "CreateSong":
+                    this.MainFrame.Navigate(typeof(Views.HomePage));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
